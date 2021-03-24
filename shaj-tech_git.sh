9a5d637e44c02e9ecf76690d3c6067fabb810ecd
julfi <julfikar.islam.khan@gmail.com>
2021-03-24 11:51:06 +0600

    BASIC GIT CMDS-SHAJtech

--- shaj-tech_git.sh
+++ shaj-tech_git.sh
@@ -1,3 +1,25 @@
 git --version #Windows CMD
 
 
+#Git Bash Here 
+git init
+
+#config
+git config --global user.name "julfi" #git config user.name "julfi" #Local
+git config --global user.email "julfikar.islam.khan@gmail.com"
+git config --list
+
+#status
+git status
+
+git add *.txt #Untracked to Tracked
+git add --all #Staging all
+git add . #Staging all
+
+#commit
+git commit
+#vi => Insert I => :x
+git commit -m "msg"
+
+git log
+git log --online #Formatted.
